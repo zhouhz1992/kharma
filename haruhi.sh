281ebfac2bc7f842eeea6fbf171f001e9afd4a8a
Hongzhe Zhou <zhouhz1992@gmail.com>
2024-03-14 21:40:43 +0800

    personal config file for haruhi

---
+++ haruhi.sh
@@ -0,0 +1,17 @@
+# Macbook with intel CPU and no GPU
+if [[ $(hostname -f) == "haruhi.local" ]]; then
+  echo "We are on haruhi"
+  
+  C_NATIVE=gcc
+  CXX_NATIVE=g++
+
+  NPROC=4
+  HOST_ARCH="ICL"
+
+  MPI_EXE="mpirun"
+fi
+
+if [[ $(hostname -f) == "mgmt" ]]; then
+  echo "We are on Astro"
+
+fi
